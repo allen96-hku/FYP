bb20f89900e1897d8acad805d72e6b700d0a19bf
Allen Ngan <allen96@hku.hk>
2017-11-29 12:38:10 +0800

    nth

--- hkunetwork/scripts/gen.sh
+++ hkunetwork/scripts/gen.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-# Generate Crptomaterial
+# Generate Cryptomaterial
 ../bin/cryptogen generate --config=./crypto-config.yaml
 
-# Generate genesis.block, 
+# Genesis block
 export FABRIC_CFG_PATH=$PWD
 mkdir channel-artifacts
 ../bin/configtxgen -profile FourOrgsOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
 
+# Channel TX
 export CHANNEL_NAME=athhall
 ../bin/configtxgen -profile FourOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID $CHANNEL_NAME
 
-
+# Anchor Peer Update TX
 ../bin/configtxgen -profile FourOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/AthClubMSPanchors.tx -channelID $CHANNEL_NAME -asOrg AthClubMSP
 ../bin/configtxgen -profile FourOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/HallAMSPanchors.tx -channelID $CHANNEL_NAME -asOrg HallAMSP
 ../bin/configtxgen -profile FourOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/HallBMSPanchors.tx -channelID $CHANNEL_NAME -asOrg HallBMSP
